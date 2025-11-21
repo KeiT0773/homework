5c1d22f2b5cb7c4577c8ce543ec7d0b06489b540
KeiT0773 <ii88849@gmail.com>
2025-11-21 23:49:58 +0900

    EC2の数を２つへ変更

--- ec2.tf
+++ ec2.tf
@@ -2,17 +2,32 @@
 variable "my_ami" {}
 
 # EC2
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec2_1" {
   ami                    = var.my_ami
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.PublicAZ1a.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
   tags = {
-    Name = "aws-study-ec2-${var.my_env}"
+    Name = "aws-study-ec2_1-${var.my_env}"
   }
 }
 
-output "ec2_id" {
-  value = aws_instance.ec2.id
+output "ec2_1_id" {
+  value = aws_instance.ec2_1.id
+}
+
+resource "aws_instance" "ec2_2" {
+  ami                    = var.my_ami
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.PublicAZ1c.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  tags = {
+    Name = "aws-study-ec2_2-${var.my_env}"
+  }
+}
+
+output "ec2_2_id" {
+  value = aws_instance.ec2_2.id
 }
